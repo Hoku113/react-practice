8cef9731fd10dce4613d997a41f499e95e1ab1a1
Hoku <hokuto.u0113@gmail.com>
2022-12-13 00:33:57 +0900

    react styling sass

---
+++ react-styling-sass/src/pages/first_sass.js
@@ -0,0 +1,12 @@
+import './style/first_sass.scss';
+
+const FirstSass = () => {
+    return (
+        <>
+            <h1>hello style</h1>
+            <p>Add a little style!</p>
+        </>
+    )
+}
+
+export default FirstSass;
